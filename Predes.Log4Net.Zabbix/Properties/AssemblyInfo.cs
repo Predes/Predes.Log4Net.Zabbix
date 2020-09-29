4cad271dbfd0ee1160870c95150c6c5c9ee67678
e-vw <e-vw@users.noreply.github.com>
2020-09-29 13:44:16 +0200

    Updated nuget packages to support Zabbix 4.0

--- Predes.Log4Net.Zabbix/Properties/AssemblyInfo.cs
+++ Predes.Log4Net.Zabbix/Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Class library with log4net appender for Zabbix.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Predes")]
-[assembly: AssemblyProduct("Predes.Log4Net.Zabbix")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("Predes.Log4Net.Zabbix" )]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
